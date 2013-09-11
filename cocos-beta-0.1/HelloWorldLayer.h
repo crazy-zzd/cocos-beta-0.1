5e816a73f70a95803d6ce66618f89872554b87f8
朱 俊健 <wisejoker@shutekiMacBook-Pro.local>
2013-09-11 21:50:32 +0800

    Initial Commit

---
+++ cocos-beta-0.1/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  cocos-beta-0.1
+//
+//  Created by 朱 俊健 on 13-9-11.
+//  Copyright 朱 俊健 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
